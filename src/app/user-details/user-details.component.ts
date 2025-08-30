86a2774bce5b19c5df49ab5d9520f91187e3202a
Michelle <mpuschkarow@web.de>
2025-08-30 11:31:55 +0200

    user detail card created

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  imports: [
+     MatCardModule,
+     MatTabsModule
+  ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
